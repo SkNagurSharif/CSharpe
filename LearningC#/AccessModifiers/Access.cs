6507dac7fd894fdf83f8b8c3cd796be6574f83d0
Shaik Nagur Sharif <sknagursharif@outlook.com>
2023-08-06 12:05:06 +0530

    File created

---
+++ LearningC#/AccessModifiers/Access.cs
@@ -0,0 +1,13 @@
+namespace Namespace;
+public class Class
+{
+/*
+Access Modifiers are also called as Access Specifiers
+
+*/
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
