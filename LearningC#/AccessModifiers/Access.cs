9149a35b67a8ba7c5d0b0ad97239fb3f643f7aee
Shaik Nagur Sharif <sknagursharif@outlook.com>
2023-09-22 10:38:51 +0530

    no changes

--- LearningC#/AccessModifiers/Access.cs
+++ LearningC#/AccessModifiers/Access.cs
@@ -1,13 +1,37 @@
 namespace Namespace;
-public class Class
+public class Access
 {
 /*
 Access Modifiers are also called as Access Specifiers
+Access modifiers are Keywords tp specify the accessibility of a class, method, field or variable
 
+1. Public
+2. Private
+3. Protected
+4. Internal
+5. Protected Internal
+
+|-------------------------------------------------------------------------------------------------------------------------------------|
+|    Keyword        |   Inside Same Assemmbly where member is declared     |  Other Assembly where containing Assembly id referenced  |
+|      or           |----------------------------------------------------- |----------------------------------------------------------|
+| Access Specifier  | Inside Same Class | Inside Derived Class | Other Code|          Inside Derived Class  |         Other code      |
+| -------------------------------------------------------------------------|----------------------------------------------------------|
+|  Public             |      Yes          |      Yes           |    Yes    |                  Yes          |          Yes             |                                             |   
+|  Private            |      Yes          |      Noooooo       | Noooooo   |             Noooooo           |     Noooooo              |
+|  Protected          |      Yes          |      Yes           |   Noooooo |                  Yes          |        Noooooo           |   
+|  Internal           |      Yes          |      Yes           |    Yes    |             Noooooo           |           Noooooo        |   
+|  Protected Internal |      Yes          |      Yes           |    Yes    |                  Yes          |              Noooooo     |   
+|-------------------------------------------------------------------------------------------------------------------------------------|
+
+Default Access modifier/ specifier is also known as " Internal"
 */
+public string hell = "Hello Nagur !";
 
+
+// Default or internal Access modifier / Specifier
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Access access = new Access();
+        Console.WriteLine(access.hell);
     }
 }
